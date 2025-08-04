8fc644e57eb22c8238f47e4d2ab5ec0844f81d9f
Iverson Khoo <khooiverson02@gmail.com>
2025-08-03 20:11:41 -0700

    Implement contact form script

--- script.js
+++ script.js
@@ -29,3 +29,21 @@ function openmenu() {
 function closemenu() {
   sideMenu.style.right = "-200px";
 }
+
+// Contact form submission handler
+const scriptURL = 'https://script.google.com/macros/s/AKfycbypP3gok-YONmbErE1IPFuSDYWFMAvaBkASRifyW0F3JA9FcaqYgqn7oO4hG9VxnLY/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+      msg.innerHTML = "Message sent successfully!";
+      setTimeout(function() {
+        msg.innerHTML = "";
+      }, 3000) // Clear message after 3 seconds
+      form.reset() // Reset the form after successful submission
+    }) 
+    .catch(error => console.error('Error!', error.message))
+})
